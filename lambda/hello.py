2810fa4eef2cc3593f73afc6aeaac28cdd8ca6fa
Van Cong Le Ca <cascabusiness@gmail.com>
2024-12-16 16:33:05 +0700

    Create lambda-cdk project

---
+++ lambda/hello.py
@@ -0,0 +1,16 @@
+import json
+        
+def handler(event, context):
+    json_name = "Ca"
+    json_company = "Rikkeisoft"
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "message": "Hello World From AWS!",
+            "name" : json_name,
+            "company" : json_company,
+        })
+    }
